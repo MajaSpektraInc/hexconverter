071ff5d0abf68f382d7801245d9f839c5056b817
maja <maja.ewertoswska.142@accesscreative.ac.uk>
2023-09-28 14:08:35 +0100

    Add project files.

---
+++ testConsole/HexConverter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testConsole
+{
+    static class HexConverter
+    {
+       public static void PrintHello()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+        public static float AddNumbers(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static int HextoInt(string s)
+        {
+            char[] chars = s.ToUpper().ToCharArray();
+            chars.Reverse();
+
+            Dictionary<char, int> lookup = new Dictionary<char, int>()
+            {
+                { 'A', 10 },
+                { 'B', 11 },
+                { 'C', 12 },
+                { 'D', 13 },
+                { 'E', 14 },
+                { 'F', 15 }
+            };
+
+            int result;
+            result = 0;
+            foreach (char c in chars)
+            {
+                int v;
+                if (lookup.ContainsKey(c))
+                {
+                    v = lookup[c];
+                }
+                else
+                {
+                    try
+                    {
+                        v = Convert.ToInt32(c);
+                        if (v > 15)
+                        {
+                            throw new Exception("");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(c + " is not an acceptable character, clamped to F");
+                        v = 15;
+                    }
+                }
+                result += v;
+            }
+
+            return result;
+        }
+
+        }
+    }
+
---
+++ testConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿//// See https://aka.ms/new-console-template for more information
+using testConsole;
+
+//Console.WriteLine("Enter your age: ");
+//int age = Convert.ToInt32(Console.ReadLine());
+
+//if (age<18)
+//{
+//    Console.WriteLine("You can't drink legally.");
+//}
+//else if (age < 21)
+//{
+//    Console.WriteLine("You can't drink legally in America.");
+//}
+//else
+//{
+//    HexConverter.PrintHello();
+//}
+
+//Console.WriteLine("Enter a number: ");
+//float a = float.Parse(Console.ReadLine());
+//Console.WriteLine("Enter another number: ");
+//float b = float.Parse(Console.ReadLine());
+//float n = HexConverter.AddNumbers(a, b);
+//Console.WriteLine(n);
+
+//Console.WriteLine("Enter a Hex");
+//String hexcode = Console.ReadLine();
+//char[] codes = hexcode.ToCharArray();
+
+//foreach(char c in codes)
+//{
+//    Console.WriteLine(c);
+//}
+
+
+Console.WriteLine("Enter a HEX");
+string hexcode = Console.ReadLine();
+
+Console.WriteLine(HexConverter.HextoInt(hexcode));
